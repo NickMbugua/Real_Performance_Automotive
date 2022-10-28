8a026575dedb73ebcec4abc86d34ef6389f22106
Your NameNick <mbugua@ualberta.ca>
2022-10-28 01:17:03 -0600

    Changed the navbar color

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,12 @@ var currentDay = currentDate.getDay();
 console.log(currentDay);
 
 const hours = ["CLOSED","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","CLOSED"];
-const days =["Sunday", "Open: Monday", "Open: Tuesday", "Open: Wednesday", "Open: Thursday", "Open: Friday", "Open: Saturday"]
 var setHour=hours[currentDay];
-var setWeekDay =days[currentDay];
 console.log(setHour);
 
 let shopHour = document.createElement("p");
 const divText = document.createTextNode(setHour);
 shopHour.appendChild(divText);
-
-
 
 let hourDiv = document.querySelector('#shop_hour');
 hourDiv.appendChild(shopHour);
