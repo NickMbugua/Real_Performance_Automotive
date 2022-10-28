716e59cebd11a5b2a9d8f113397ddae1a4e4f41f
Your NameNick <mbugua@ualberta.ca>
2022-10-28 01:26:25 -0600

    Changed flex on store page image. Formatted all pages.

--- src/js/contact.js
+++ src/js/contact.js
@@ -8,7 +8,7 @@ let reason = document.querySelector("#reason");
 let errorDiv = document.querySelector("#error-message");
 let submitButton = document.querySelector("#submit");
 let regEx = (/^[a-z A-Z()-]+$/);
-let phoneRegEx =(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/)
+let phoneRegEx = (/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/)
 var currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
@@ -19,51 +19,47 @@ let yearError = document.createTextNode("Invalid Year");
 
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    let message =[];
-    if (userName.value == "" ) {
+    let message = [];
+    if (userName.value == "") {
         console.log("Error");
         message.push("Please add your full name!");
     }
-    if (phoneNumber.value == "" ) {
+    if (phoneNumber.value == "") {
         console.log("Error");
         message.push("Please add your phone number!");
     }
-    if (!userName.value.match(regEx))
-    {
+    if (!userName.value.match(regEx)) {
         message.push("Please entry an valid name!");
     }
-    if(!phoneNumber.value.match(phoneRegEx))
-    {
+    if (!phoneNumber.value.match(phoneRegEx)) {
         message.push("Please entry an valid Phone Number!");
     }
-   
-     if ((year.value) > currentYear) {
+
+    if ((year.value) > currentYear) {
         console.log(year.value);
         message.push("Please entry an valid Year!");
 
     }
-    if (!make.value.match(regEx))
-    {
+    if (!make.value.match(regEx)) {
         message.push("Please entry an valid vehicle manufacturer!");
     }
-    if (!make.value.match(/^[a-zA-Z 0-9() -]+$/))
-    {
+    if (!make.value.match(/^[a-zA-Z 0-9() -]+$/)) {
         message.push("Please entry an valid vehicle model name!");
     }
-    if (message.length >0){
+    if (message.length > 0) {
         e.preventDefault();
         errorDiv.innerHTML = message.join('</br>')
     }
     else {
         window.location.href = `mailto:mbugua@ualberta.ca?subject="${userName.value.trim()} ${year.value.trim()} ${make.value.trim()} ${model.value.trim()}"&body=%0DPhone Number: ${phoneNumber.value.trim()}%0DClient: ${userName.value.trim()}%0DRequest: ${reason.value}`;
         errorDiv.innerHTML = "";
-        userName.value="";
-        phoneNumber.value='';
-        year.value="";
-        make.value ="";
-        model.value="";
-        reason.value="";
-       
+        userName.value = "";
+        phoneNumber.value = '';
+        year.value = "";
+        make.value = "";
+        model.value = "";
+        reason.value = "";
+
     }
 
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 var currentDate = new Date();
-new Date().toLocaleString('en-US', {timeZone: 'America/Los_Angeles'})
+new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })
 var currentDay = currentDate.getDay();
 
 console.log(currentDay);
 
-const hours = ["CLOSED","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","Open: 8:00 AM - 6:00 PM","CLOSED"];
-var setHour=hours[currentDay];
+const hours = ["CLOSED", "Open: 8:00 AM - 6:00 PM", "Open: 8:00 AM - 6:00 PM", "Open: 8:00 AM - 6:00 PM", "Open: 8:00 AM - 6:00 PM", "Open: 8:00 AM - 6:00 PM", "CLOSED"];
+var setHour = hours[currentDay];
 console.log(setHour);
 
 let shopHour = document.createElement("p");
